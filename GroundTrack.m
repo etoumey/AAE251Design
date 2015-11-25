6588553e26cc7b65a6eb6debd79b845d7d597e79
etoumey <etoumey@gmail.com>
2015-11-25 14:12:22 -0500

    AAE251 Design first commit

---
+++ GroundTrack.m
@@ -0,0 +1,34 @@
+clear
+clc
+
+B = 120;  %[degrees] N launch azimuth
+Lat = 28.4556;  %[degrees] N launch latitude 
+Long = 80.5278;  %[degrees] W launch longitude
+LatRad = Lat*2*pi/360;
+LongRad = Long*2*pi/360;
+re = 6378;  %[km] radius of Earth
+ra = 1000+re;  %[km] apoapsis radius
+rp = 1000+re;  %[km] periapsis radius
+u = 3.986E5;  %km^3/s^2
+
+
+
+P = (2*pi*ra^(3/2))/sqrt(u);
+P = P/60/60;
+i = acosd(sind(B)*cosd(Lat));  %[degrees] inclination
+iRad = (i/360)*2*pi;
+phaseShift = asin(LatRad/iRad)-(P*LongRad);
+
+
+figure;
+imshow('map.png', 'XData', [0 2*pi], 'YData', [pi/2 -pi/2]);
+impoint(gca, LongRad, LatRad);
+set(gca,'Ydir','Normal');
+hold on
+for rotation = 1:10
+    plot(linspace(0,2*pi,10000), (iRad*sin(linspace(0,2*pi,10000)*P+phaseShift)),'Color', rand(1,3), 'LineWidth', 2); 
+    phaseShift = phaseShift + (P/(24));
+    hold on
+end
+
+hold off
---
+++ SRSLanding.m
@@ -0,0 +1,61 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%            AAE 251 Design Project               %
+%                                                 % 
+%    Team: 6                     Author: Eliot    %
+%                                                 %
+%                  SRS Orbit info                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc
+clear
+%%%%%%%%%%%%%%%%%% Constants %%%%%%%%%%%%%%%%%%%%%%
+
+mPay = 12000;  %kg of SRS
+re = 6378;  %km radius of Earth
+u = 3.986E5;  %km^3/s^2
+Isp = 316;  %Isp of OMS system on Shuttle
+ic = 30;  %inclination at checkout
+
+%%%%%%%%%%%%%%%% Calculations %%%%%%%%%%%%%%%%%%%%%
+
+ro = 200 + re;  %initial checkout orbit
+vo = sqrt(u/ro);  %initial orbital velocity
+raTrans = 1000 + re;
+rpTrans = ro;
+
+a = .5*(raTrans+rpTrans);
+
+deltaV1 = sqrt(2*((u/ro)-(u/(2*a)))) - vo;
+
+deltaV2 = sqrt(u/raTrans) - sqrt(2*((u/raTrans)-(u/(2*a))));
+
+r1 = 200;
+r2 = 1000;
+e = ((raTrans+rpTrans)/raTrans)
+
+theta = [0:0.01:2*pi];
+polar(theta, r1+(theta*0))
+hold on
+%polar(theta, raTrans*sin(theta)+rpTrans*cos(theta))
+%hold on
+polar(theta, r2+(theta*0))
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%% Deorbit %%%%%%%%%%%%%%%%%%%%%%
+ro = 1000+re;
+ra = 1000+re;
+rp = re;
+vo = sqrt(u/ro);
+
+a = .5*(ra+rp);
+
+deltaV3 = abs(sqrt(2*((u/ro)-(u/(2*a)))) - vo);
+
+deltaV = deltaV1 + deltaV2 + deltaV3;
